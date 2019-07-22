61b45d9df182233ebe45e1c6c5d2b40f9954ada2
Wesley Weisenberger <wesleynweisenberger@gmail.com>
2019-07-22 13:43:09 -0700

    added instruction

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+#begin 
 
+
+class BattleShipGame():
+    #create 3d matrix
+
+
+
+    
+    #function to place AI ships
+
+
+
+    #function to let user choose coordinates of ship to hit
+
+
+
+    #function to let user place ships
+
+
+
+    #function to see if the user hits the ships
+
+
+
+    #data structure to store ships and hits
+
+
+
+    #function to see if ship is sunk
+
+
+
+    #function to start game
